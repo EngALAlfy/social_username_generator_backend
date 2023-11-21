2f2a80e1565e7ff6e8db90257844101318537c02
alalfy <doctorviruos@gmail.com>
2023-11-21 21:29:06 +0200

    add favicon

--- admin/pages/change_password.php
+++ admin/pages/change_password.php
@@ -12,6 +12,10 @@ check_user();
 
 <head>
     <meta charset="UTF-8">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="../favicon.png">
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
